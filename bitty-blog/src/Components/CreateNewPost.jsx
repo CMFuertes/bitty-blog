c03b99321bd15a638421fbb9053a179af9c613ef
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-04-09 20:31:41 -0400

    CreateNewPost completed

--- bitty-blog/src/Components/CreateNewPost.jsx
+++ bitty-blog/src/Components/CreateNewPost.jsx
@@ -3,8 +3,17 @@ const CreateNewPost = () => {
     return (
         <>
     <form>
-        
+            <h1>Create New Post</h1>
+            <input type ="text" placeHolder="title" size="39" required></input>
+            <br />
+            <br />
+            <textarea placeHolder="contents" rows="8" cols="41" required></textarea>
+            <br />
+            <br />
+            <button>Save Post</button>
+
     </form>
+    </>
     );
 };
 
