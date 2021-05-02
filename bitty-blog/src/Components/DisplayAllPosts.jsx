06cd8c397440ec9ed0c2aff23fc862de796e4d37
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-05-01 23:25:40 -0400

    modified to display post data

--- bitty-blog/src/Components/DisplayAllPosts.jsx
+++ bitty-blog/src/Components/DisplayAllPosts.jsx
@@ -4,6 +4,7 @@ const DisplayAllPosts = () => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [allPosts, setAllPosts] = useState([]);
+    const [isCreateNewPost, setisCreateNewPost] = useState(false);
 
     const getTitle = useRef();
     const getContent = useRef();
@@ -21,10 +22,11 @@ const DisplayAllPosts = () => {
     const savePost = event => {
         event.preventDefault();
         const id = Date.now();
-        setAllPost([...allPosts, {title, content, id}]);
+        setAllPost([...allPosts, {title, content }]);
         console.log(allPosts);
         getTitle.current.value = "";
         getContent.current.value = "";
+        toggleCreateNewPost();
     };
     if(isCreateNewPost){
         return (
@@ -42,6 +44,22 @@ const DisplayAllPosts = () => {
 return (
     <>
     <h2>ALl Posts</h2>
+    {!allPosts.length ? (
+        <div>
+          <h3>There is nothing to see here!</h3>
+        </div>
+      ) : (
+        allPosts.map(eachPost => {
+          return (
+            <Post
+              id={eachPost.id}
+              key={eachPost.id}
+              title={eachPost.title}
+              content={eachPost.content}
+            />
+          );
+        })
+      )}
     <br/>
     <br/>
     <button onClock={toggleCreateNewPost}>Create New</button>
