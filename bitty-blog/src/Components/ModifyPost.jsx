3909c9d151e04aa8a1b49f2ff272a046b2755488
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-05-01 23:45:36 -0400

    set default value of inputs that will be rendered to the user

--- bitty-blog/src/Components/ModifyPost.jsx
+++ bitty-blog/src/Components/ModifyPost.jsx
@@ -3,16 +3,27 @@ const ModifyPost = () => {
     return (
         <>
             <form>
-                <h1>Modify Post</h1>
-                <input type ="text" placeholder="title" size ="39" required></input>
-                <br />
-                <br />
-                <textarea placeholder="contents" rows="8" cols="41" required></textarea>
-                <br />
-                <br />
-                <button>Update Post</button>
-            </form>
-        </>
-    );
+                <h1>Modify Post</h1>        <input
+          defaultValue={props.title}
+          onChange={props.savePostTitleToState}
+          text
+          placeholder="title"
+          size="39"
+        ></input>
+        <br />
+        <br />
+        <textarea
+          defaultValue={props.content}
+          placeholder="contents"
+          onChange={props.savePostContentToState}
+          rows="8"
+          cols="41"
+        ></textarea>
+        <br />
+        <br />
+        <button onClick ={props.updatePost}>Update Post</button>
+      </form>
+    </>
+  );
 };
 export default ModifyPost;
